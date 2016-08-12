77b214c8763524878867cdf65e444c7357ba0bad
Jared Parsons <jaredpar@microsoft.com>
2016-08-12 16:29:46 -0700

    Simple testing setup
    Got the testing story going.  Added a test to verify that #1 is actually a bug.

---
+++ src/SignToolTests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SignTool.UnitTests
+{
+    public class ProgramTests
+    {
+        public class ReadConfigFileTests : ProgramTests
+        {
+            private BatchSignInput Load(string json)
+            {
+                using (var reader = new StringReader(json))
+                using (var writer = new StringWriter())
+                {
+                    BatchSignInput data;
+                    Assert.True(Program.TryReadConfigFile(writer, reader, @"q:\outputPath", out data));
+                    Assert.True(string.IsNullOrEmpty(writer.ToString()));
+                    return data;
+                }
+            }
+
+            [Fact]
+            public void MissingExcludeSection()
+            {
+                var json = @"
+{
+    ""sign"": []
+}";
+                var data = Load(json);
+                Assert.Equal(0, data.BinaryNames.Length);
+                Assert.Equal(0, data.ExternalBinaryNames.Length);
+            }
+        }
+    }
+}
