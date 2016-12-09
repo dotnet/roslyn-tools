f6c4838da6858d80dc5cbf947c923c157a07f268
Tanner Gooding <tagoo@microsoft.com>
2016-12-08 16:45:25 -0800

    Updating the LocateVS package to include the native query api binaries.

--- src/LocateVS/LocateVS/Interop.cs
+++ src/LocateVS/LocateVS/Interop.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Setup.Configuration;
 
@@ -9,7 +10,22 @@ namespace LocateVS
     {
         public const int REGDB_E_CLASSNOTREG = unchecked((int)0x80040154);
 
-        [DllImport("Microsoft.VisualStudio.Setup.Configuration.Native.dll")]
-        public static extern void GetSetupConfiguration([MarshalAs(UnmanagedType.Interface)] out ISetupConfiguration setupConfiguration);
+        [DllImport("Microsoft.VisualStudio.Setup.Configuration.Native.x64.dll")]
+        public static extern void GetSetupConfiguration_x64([MarshalAs(UnmanagedType.Interface)] out ISetupConfiguration setupConfiguration);
+
+        [DllImport("Microsoft.VisualStudio.Setup.Configuration.Native.x86.dll")]
+        public static extern void GetSetupConfiguration_x86([MarshalAs(UnmanagedType.Interface)] out ISetupConfiguration setupConfiguration);
+
+        public static void GetSetupConfiguration(out ISetupConfiguration setupConfiguration)
+        {
+            if (Environment.Is64BitProcess)
+            {
+                GetSetupConfiguration_x64(out setupConfiguration);
+            }
+            else
+            {
+                GetSetupConfiguration_x86(out setupConfiguration);
+            }
+        }
     }
 }
