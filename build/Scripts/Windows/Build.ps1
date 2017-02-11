d9e35eea501021f479b49cf6a879aaf65103a24a
Tomas Matousek <tomat@microsoft.com>
2017-02-10 17:24:23 -0800

    Update package version to 0.3.1-beta

--- build/Scripts/Windows/Build.ps1
+++ build/Scripts/Windows/Build.ps1
@@ -8,7 +8,7 @@ Param(
   [string] $msbuildVersion = "15.0",
   [string] $nugetVersion = "3.6.0-beta1",
   [switch] $official,
-  [string] $publishedPackageVersion = "0.2.4-beta",
+  [string] $publishedPackageVersion = "0.3.1-beta",
   [switch] $realSign,
   [string] $signToolVersion = "0.2.4-beta",
   [switch] $skipBuild,
