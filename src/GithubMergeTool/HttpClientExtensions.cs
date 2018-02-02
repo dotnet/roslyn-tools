54de10409448dee481fe07f632e966ad315dfb7f
Andy Gocke <angocke@microsoft.com>
2018-02-01 16:22:10 -0800

    Add GithubMergeTool to roslyn-tools

---
+++ src/GithubMergeTool/HttpClientExtensions.cs
@@ -0,0 +1,15 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubMergeTool
+{
+    internal static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsyncAsJson(this HttpClient client, string requestUri, string body)
+            => client.PostAsync(requestUri, new StringContent(body, Encoding.UTF8, "application/json"));
+
+        public static Task<HttpResponseMessage> PutAsyncAsJson(this HttpClient client, string requestUri, string body)
+            => client.PutAsync(requestUri, new StringContent(body, Encoding.UTF8, "application/json"));
+    }
+}
