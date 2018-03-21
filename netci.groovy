be1d6fef33c3f74331b005fc9d806ae356ba5e26
Tomáš Matoušek <tmat@users.noreply.github.com>
2018-03-21 11:16:08 -0700

    Fix netci.groovy

--- netci.groovy
+++ netci.groovy
@@ -69,7 +69,7 @@ static addBuildSteps(def job, def projectName, def os, def configName, def isPR)
       addXUnitDotNETResults(myJob, configName)
 
       if (os == 'Windows_NT') {
-        Utilities.setMachineAffinity(myJob, 'latest-dev15-3')  
+        Utilities.setMachineAffinity(myJob, os, 'latest-dev15-3')  
       } else {
         Utilities.setMachineAffinity(myJob, os, 'latest-or-auto')
       }
