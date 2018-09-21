993f7b6f42a7eacad3bbfc62dff2b9a0b4f4e58e
Jonathon Marolf <jmarolf@microsoft.com>
2018-09-21 13:56:14 -0700

    use new commandline parser and enable signing

--- src/OptProf/roslyn.optprof/Program.cs
+++ src/OptProf/roslyn.optprof/Program.cs
@@ -1,131 +1,104 @@
-using Mono.Options;
+using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using roslyn.optprof.json;
 using roslyn.optprof.lib;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace roslyn.optprof
 {
     internal static class Program
     {
-        public static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            bool showHelp = false;
-            string configFile = null;
-            string rootFolder = null;
-            string outputFolder = null;
-
-            var parser = new OptionSet
-            {
-                {
-                    "h|?|help",
-                    "Show help.",
-                    h => showHelp = h != null
-                },
-                {
-                    "c|config=",
+            var parser = new CommandLineBuilder()
+                .UseParseDirective()
+                .UseHelp()
+                .UseSuggestDirective()
+                .UseParseErrorReporting()
+                .UseExceptionHandler()
+                .AddOption(
+                    new[] { "-c", "--configFile" },
                     "The absolute path to the OptProf.json config file.",
-                    c => configFile = c
-                },
-                {
-                    "if=|insertionfolder=",
+                    c => c.WithDefaultValue(() => null).ParseArgumentsAs<string>())
+                .AddOption(
+                    new[] { "-if", "--insertionFolder" },
                     "This is the absolute path to the folder that contains the VSIXes that will be inserted.",
-                    i => rootFolder = i
-                },
-                {
-                    "o=|output=",
-                    $"The folder to output the results optprof data to.",
-                    o => outputFolder = o
-                },
-            };
+                    i => i.WithDefaultValue(() => null).ParseArgumentsAs<string>())
+                .AddOption(
+                    new[] { "-o", "--outputFolder" },
+                    "The folder to output the results optprof data to.",
+                    o => o.WithDefaultValue(() => null).ParseArgumentsAs<string>())
+                .AddVersionOption()
+                .OnExecute(typeof(Program).GetMethod(nameof(Execute)))
+                .Build();
 
-            List<string> extraArguments = null;
-            try
-            {
-                extraArguments = parser.Parse(args);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Failed to parse arguments.");
-                Console.WriteLine(e.Message);
-                return;
-            }
-
-            if (extraArguments.Count > 0)
-            {
-                Console.WriteLine($"Unknown arguments: {string.Join(" ", extraArguments)}");
-                return;
-            }
-
-            if (showHelp)
-            {
-                parser.WriteOptionDescriptions(Console.Out);
-                return;
-            }
-
-            if (configFile == null)
-            {
-                Console.WriteLine($"Must specify '-config'");
-                parser.WriteOptionDescriptions(Console.Out);
-                return;
-            }
-
-            if (rootFolder == null)
-            {
-                Console.WriteLine($"Must specify '-insertionfolder'");
-                parser.WriteOptionDescriptions(Console.Out);
-                return;
-            }
-
-            if (outputFolder == null)
-            {
-                Console.WriteLine($"Must specify '-output'");
-                parser.WriteOptionDescriptions(Console.Out);
-                return;
-            }
-
-            Execute(configFile, rootFolder, outputFolder);
+            return await parser.InvokeAsync(args);
         }
 
-        private static void Execute(string configFile, string rootFolder, string outputFolder)
+        private static void Execute(string configFile, string insertionFolder, string outputFolder, IConsole console = null)
         {
+            if (string.IsNullOrEmpty(configFile))
+            {
+                throw new ArgumentException($"must specify '--{nameof(configFile)}'", nameof(configFile));
+            }
+
+            if (string.IsNullOrEmpty(insertionFolder))
+            {
+                throw new ArgumentException($"must specify '--{nameof(insertionFolder)}'", nameof(insertionFolder));
+            }
+
+            if (string.IsNullOrEmpty(outputFolder))
+            {
+                throw new ArgumentException($"must specify '--{nameof(outputFolder)}'", nameof(outputFolder));
+            }
+
             var config = ReadConfigFile(configFile);
 
             foreach (var product in config.Products)
             {
                 string productName = product.Name;
-                string path = Path.Combine(rootFolder, productName);
-                using (var vsix = Vsix.Create(path))
-                {
-                    var jsonManifest = vsix.ParseJsonManifest("/manifest.json");
-                    var fileEntries = Manifest.GetNgenEntriesFromJsonManifest(jsonManifest).ToArray();
-                    foreach (var test in product.Tests)
-                    {
-                        var folder = Path.Combine(outputFolder, test.Container);
-                        var configurations = Path.Combine(folder, "Configurations");
-                        foreach (var fullyQualifiedName in test.TestCases)
-                        {
-                            var folderToWriteJsonEntires = Path.Combine(configurations, fullyQualifiedName);
-                            if (!Directory.Exists(folderToWriteJsonEntires))
-                            {
-                                Directory.CreateDirectory(folderToWriteJsonEntires);
-                            }
+                string path = Path.Combine(insertionFolder, productName);
+                var jsonManifest = GetJsonManifest(path);
+                var fileEntries = Manifest.GetNgenEntriesFromJsonManifest(jsonManifest).ToArray();
 
-                            foreach (var entry in fileEntries)
-                            {
-                                var filename = Path.GetFileNameWithoutExtension(entry.RelativeInstallationPath) + ".IBC.json";
-                                using (var writer = new StreamWriter(File.Open(Path.Combine(folderToWriteJsonEntires, filename), FileMode.Create, FileAccess.Write, FileShare.Read)))
-                                {
-                                    string jsonString = ToJsonString(entry);
-                                    writer.WriteLine(jsonString);
-                                }
-                            }
+                foreach (var test in product.Tests)
+                {
+                    var folder = Path.Combine(outputFolder, test.Container);
+                    var configurations = Path.Combine(folder, "Configurations");
+                    foreach (var fullyQualifiedName in test.TestCases)
+                    {
+                        var folderToWriteJsonEntires = Path.Combine(configurations, fullyQualifiedName);
+                        if (!Directory.Exists(folderToWriteJsonEntires))
+                        {
+                            Directory.CreateDirectory(folderToWriteJsonEntires);
                         }
+
+                        WriteEntries(fileEntries, folderToWriteJsonEntires);
                     }
                 }
+            }
+        }
+
+        private static void WriteEntries((string Technology, string RelativeInstallationPath, string InstrumentationArguments)[] fileEntries, string folderToWriteJsonEntires)
+        {
+            foreach (var entry in fileEntries)
+            {
+                var filename = Path.GetFileNameWithoutExtension(entry.RelativeInstallationPath) + ".IBC.json";
+                filename = Path.Combine(folderToWriteJsonEntires, filename);
+                WriteEntry(filename, entry);
+            }
+        }
+
+        private static JObject GetJsonManifest(string path)
+        {
+            using (var vsix = Vsix.Create(path))
+            {
+                return vsix.ParseJsonManifest("/manifest.json");
             }
         }
 
@@ -137,10 +110,19 @@ namespace roslyn.optprof
                 if (!success)
                 {
                     // handle error case
-                    throw new Exception("Unable to open the config file");
+                    throw new Exception($"Unable to open the config file '{pathToConfigFile}'");
                 }
 
                 return config;
+            }
+        }
+
+        private static void WriteEntry(string filename, (string Technology, string RelativeInstallationPath, string InstrumentationArguments) entry)
+        {
+            using (var writer = new StreamWriter(File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read)))
+            {
+                string jsonString = ToJsonString(entry);
+                writer.WriteLine(jsonString);
             }
         }
 
