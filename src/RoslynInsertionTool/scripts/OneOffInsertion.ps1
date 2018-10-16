38c5c72afe491fd7b4df921ce6478f6520716c5c
Brett V. Forsgren <brettfo@microsoft.com>
2018-10-16 14:52:41 -0700

    allow more scripts to be used by ADO release pipeline

--- src/RoslynInsertionTool/scripts/OneOffInsertion.ps1
+++ src/RoslynInsertionTool/scripts/OneOffInsertion.ps1
@@ -1,37 +1,125 @@
-##########################################################################
-# Changes to this file will NOT be automatically deployed to the server. #
-#                                                                        #
-# Changes should be made on both the server and in source control.       #
-##########################################################################
+param([string] $enlistmentPath,
+      [string] $clientId,
+      [string] $clientSecret,
+      [string] $requiredValueSentinel,
+      [string] $defaultValueSentinel,
+      [string] $buildQueueName,
+      [string] $componentBranchName,
+      [string] $componentName,
+      [string] $dropPath,
+      [string] $insertCore,
+      [string] $insertDevDiv,
+      [string] $insertToolset,
+      [string] $queueValidation,
+      [string] $updateAssemblyVersions,
+      [string] $updateCoreXTLibraries,
+      [string] $visualStudioBranchName)
 
-. .\RitMutexHelper.ps1
-ExitOnNoEnlistment -pause $true
-# at this point `$EnlistmentPath` contains the enlistment path to use
+# check for unspecified required values
 
-Write-Host "PERFORM MANUAL INSERTION"
-Write-Host ""
-Write-Host "Supported Component Names:"
-Write-Host "    Roslyn"
-Write-Host "    Live Unit Testing"
-Write-Host "    VS Unit Testing"
-Write-Host "    Project System"
-Write-Host "    F#"
-$componentName = Read-Host -Prompt "Component name to insert"
-$bq = Read-Host -Prompt "Build queue name"
-$bn = Read-Host -Prompt "Component branch name"
-
-Write-Host ""
-$vsbn = Read-Host -Prompt "Visual Sudio branch name (insertion destination)"
-
-$ic = "true"
-if (($componentName -eq "Live Unit Testing") -or ($componentName -eq "Project System") -or ($componentName -eq "F#")) {
-    $ic = "false"
+$requiredValues = @(
+    @($componentName, "ComponentName"),
+    @($componentBranchName, "ComponentBranchName"),
+    @($visualStudioBranchName, "VisualStudioBranchName")
+)
+foreach ($var in $requiredValues) {
+    if (($var[0] -eq "") -or ($var[0] -eq $requiredValueSentinel)) {
+        Write-Host "Missing required value: $var[1]"
+    }
 }
 
-$id = "false"
-# only rarely will DevDiv packages be inserted, and only for Roslyn
+# process default values
+function IsDefaultValue([string] $value) {
+    return ($value -eq "") -or ($value -eq $defaultValueSentinel)
+}
 
-cd E:\prebuilt\roslyn-tools\RIT
-Write-Host "Executing: & .\RIT.exe `"/in=$componentName`" `"/bn=$bn`" `"/vsbn=$vsbn`" `"/bq=$bq`" /ic=$ic /id=$id /qv=true /u=vslsnap@microsoft.com `"/ep=$EnlistmentPath`""
-                       & .\RIT.exe  "/in=$componentName"   "/bn=$bn"   "/vsbn=$vsbn"   "/bq=$bq"  /ic=$ic /id=$id /qv=true /u=vslsnap@microsoft.com  "/ep=$EnlistmentPath"
-pause
+# $buildQueueName
+if (IsDefaultValue $buildQueueName) {
+    switch ($componentName) {
+        "F#" { $buildQueueName = "FSharp-Signed"; break }
+        "Live Unit Testing" { $buildQueueName = "TestImpact-Signed"; break }
+        "Project System" { $buildQueueName = "DotNet-Project-System"; break }
+        "Roslyn" { $buildQueueName = "Roslyn-Signed"; break }
+        "VS Unit Testing" { $buildQueueName = "VSUnitTesting-Signed"; break }
+        default {
+            Write-Host "Unable to determine BuildQueueName from ComponentName"
+            exit 1
+        }
+    }
+}
+
+# $dropPath
+if (IsDefaultValue $dropPath) {
+    switch ($componentName) {
+        "F#" { $dropPath = "\\cpvsbuild\drops\FSharp"; break }
+        "VS Unit Testing" { $dropPath = "server"; break }
+        default { $dropPath = ""; break }
+    }
+}
+
+# $insertCore
+if (IsDefaultValue $insertCore) {
+    if (($componentName -eq "Live Unit Testing") -or ($componentName -eq "Project System") -or ($componentName -eq "F#")) {
+        $insertCore = "false"
+    }
+    else {
+        $insertCore = "true"
+    }
+}
+
+# $insertDevDiv
+if (IsDefaultValue $insertDevDiv) {
+    $insertDevDiv = "false"
+}
+
+# $insertToolset
+if (IsDefaultValue $insertToolset) {
+    if (($componentName -eq "Roslyn") -and ($visualStudioBranchName.StartsWith("lab/"))) {
+        $insertToolset = "true"
+    }
+    else {
+        $insertToolset = "false"
+    }
+}
+
+# $queueValidation
+if (IsDefaultValue $queueValidation) {
+    if ($visualStudioBranchName -eq "lab/ml") {
+        $queueValidation = "false"
+    }
+    else {
+        $queueValidation = "true"
+    }
+}
+
+# $updateAssemblyVersions
+if (IsDefaultValue $updateAssemblyVersions) {
+    if ($visualStudioBranchName.StartsWith("rel/")) {
+        $updateAssemblyVersions = "false"
+    }
+    else {
+        $updateAssemblyVersions = "true"
+    }
+}
+
+# $updateCoreXTLibraries
+if (IsDefaultValue $updateCoreXTLibraries) {
+    if ($componentName -eq "Roslyn") {
+        $updateCoreXTLibraries = "true"
+    }
+    else {
+        $updateCoreXTLibraries = "false"
+    }
+}
+
+$dropPathFlag = ""
+if ($dropPath) {
+    $dropPathFlag = "/dp=$dropPath"
+}
+
+$toolsetFlag = ""
+if ($insertToolset -eq "true") {
+    $toolsetFlag = "/t"
+}
+
+& .\RIT.exe  "/in=$componentName" "/bn=$componentBranchName" "/bq=$buildQueueName" "/vsbn=$visualStudioBranchName" "/ic=$insertCore" "/id=$insertDevDiv" "/qv=$queueValidation" "/ua=$updateAssemblyVersions" "/uc=$updateCoreXTLibraries" "/u=vslsnap@microsoft.com" "/ci=$clientId" "/cs=$clientSecret" "/ep=$enlistmentPath" $toolsetFlag $dropPathFlag
