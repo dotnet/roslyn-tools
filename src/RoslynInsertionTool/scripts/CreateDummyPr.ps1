aef0bdd7f56d3e95532c96ea8bcfb5a80a5789ec
Brett V. Forsgren <brettfo@microsoft.com>
2018-10-17 09:48:01 -0700

    allow more scripts to be used by ADO release pipeline (#374)

--- src/RoslynInsertionTool/scripts/CreateDummyPr.ps1
+++ src/RoslynInsertionTool/scripts/CreateDummyPr.ps1
@@ -1,5 +1,7 @@
 param([string] $enlistmentPath,
+      [string] $clientId,
+      [string] $clientSecret,
       [string] $componentName,
       [string] $visualStudioBranchName)
 
-& .\RIT.exe  "/in=$componentName" "/vsbn=$visualStudioBranchName" /createdummypr /u=vslsnap@microsoft.com "/ep=$enlistmentPath"
+& .\RIT.exe  "/in=$componentName" "/vsbn=$visualStudioBranchName" /createdummypr /u=vslsnap@microsoft.com "/ep=$enlistmentPath" "/ci=$clientId" "/cs=$clientSecret"
--- src/RoslynInsertionTool/scripts/UpdateExistingPr.ps1
+++ src/RoslynInsertionTool/scripts/UpdateExistingPr.ps1
@@ -1,22 +1,63 @@
 param([string] $enlistmentPath,
+      [string] $clientId,
+      [string] $clientSecret,
+      [string] $requiredValueSentinel,
+      [string] $defaultValueSentinel,
       [string] $componentName,
       [string] $buildQueueName,
       [string] $componentBranchName,
-      [string] $visualStudioBranchName,
       [string] $existingPr,
-      [bool] $insertComponent,
-      [bool] $insertDevDiv)
+      [string] $insertCore,
+      [string] $insertDevDiv,
+      [string] $visualStudioBranchName)
 
-if ($insertComponent) {
-    $icFlag = "true"
-} else {
-    $icFlag = "false"
+# check for unspecified required values
+
+$requiredValues = @(
+    @($componentName, "ComponentName"),
+    @($componentBranchName, "ComponentBranchName"),
+    @($visualStudioBranchName, "VisualStudioBranchName"),
+    @($existingPr, "ExistingPR")
+)
+foreach ($var in $requiredValues) {
+    if (($var[0] -eq "") -or ($var[0] -eq $requiredValueSentinel)) {
+        Write-Host "Missing required value: $var[1]"
+    }
 }
 
-if ($insertDevDiv) {
-    $idFlag = "true"
-} else {
-    $idFlag = "false"
+# process default values
+function IsDefaultValue([string] $value) {
+    return ($value -eq "") -or ($value -eq $defaultValueSentinel)
 }
 
-& .\RIT.exe  "/in=$componentName" "/bn=$componentBranchName" "/vsbn=$visualStudioBranchName" "/bq=$buildQueueName" /ic=$icFlag /id=$idFlag /qv=true /updateexistingpr=$existingPr /u=vslsnap@microsoft.com "/ep=$enlistmentPath"
+# $buildQueueName
+if (IsDefaultValue $buildQueueName) {
+    switch ($componentName) {
+        "F#" { $buildQueueName = "FSharp-Signed"; break }
+        "Live Unit Testing" { $buildQueueName = "TestImpact-Signed"; break }
+        "Project System" { $buildQueueName = "DotNet-Project-System"; break }
+        "Roslyn" { $buildQueueName = "Roslyn-Signed"; break }
+        "VS Unit Testing" { $buildQueueName = "VSUnitTesting-Signed"; break }
+        default {
+            Write-Host "Unable to determine BuildQueueName from ComponentName"
+            exit 1
+        }
+    }
+}
+
+# $insertCore
+if (IsDefaultValue $insertCore) {
+    if (($componentName -eq "Live Unit Testing") -or ($componentName -eq "Project System") -or ($componentName -eq "F#")) {
+        $insertCore = "false"
+    }
+    else {
+        $insertCore = "true"
+    }
+}
+
+# $insertDevDiv
+if (IsDefaultValue $insertDevDiv) {
+    $insertDevDiv = "false"
+}
+
+& .\RIT.exe  "/in=$componentName" "/bn=$componentBranchName" "/vsbn=$visualStudioBranchName" "/bq=$buildQueueName" "/ic=$insertCore" "/id=$insertDevDiv" /qv=true "/updateexistingpr=$existingPr" /u=vslsnap@microsoft.com "/ep=$enlistmentPath" "/ci=$clientId" "/cs=$clientSecret"
