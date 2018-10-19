bbfd52fb3285b1637ead9c9077d271857c8f4763
Shyam N <shyamnamboodiripad@users.noreply.github.com>
2018-10-18 17:39:05 -0700

    Switch 15.9 insertion for LUT
    Keep insertion for LUT fixed at the dev15.9-preview3 branch as we don't expect to make any more changes for 15.9.

--- src/RoslynInsertionTool/scripts/VS-Insertion.ps1
+++ src/RoslynInsertionTool/scripts/VS-Insertion.ps1
@@ -51,7 +51,7 @@ Do-Insertion -component "F#" -queueName "FSharp-Signed" -fromBranch "dev16.0" -t
 
 # Dev15.9 Preview 4
 Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "dev15.9.x-vs-deps" -toBranch "rel/d15.9" -insertToolset "false" -insertDevdiv "false" -queueValidation "true" -updatecorextlibraries "true"
-Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev15.9.x"         -toBranch "rel/d15.9" -insertCore "false"    -insertDevdiv "false" -queueValidation "true"
+Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev15.9-preview3"  -toBranch "rel/d15.9" -insertCore "false"    -insertDevdiv "false" -queueValidation "true"
 Do-Insertion -component "Project System"    -queueName "DotNet-Project-System" -fromBranch "dev15.9.x"         -toBranch "rel/d15.9" -insertCore "false"    -insertDevdiv "false" -queueValidation "true" -updateassemblyversions "false"
 Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev15.9.x"         -toBranch "rel/d15.9" -insertCore "true"                           -queueValidation "true" -dropPath "server"
 
