422b6afa772809c2573b63a46d36bde6d8f91ee1
Brett V. Forsgren <brettfo@microsoft.com>
2018-10-31 15:17:06 -0700

    write the pull request URL to a file

--- src/RoslynInsertionTool/scripts/CreateDummyPr.ps1
+++ src/RoslynInsertionTool/scripts/CreateDummyPr.ps1
@@ -2,11 +2,12 @@ param([string] $enlistmentPath,
       [string] $clientId,
       [string] $clientSecret,
       [string] $componentName,
-      [string] $visualStudioBranchName)
+      [string] $visualStudioBranchName,
+      [string] $writePullRequest)
 
 . .\HelperFunctions.ps1
 
 EnsureRequiredValue -friendlyName "ComponentName" -value $componentName
 EnsureRequiredValue -friendlyName "VisualStudioBranchName" -value $visualStudioBranchName
 
-& .\RIT.exe "/in=$componentName" "/vsbn=$visualStudioBranchName" /createdummypr "/u=vslsnap@microsoft.com" "/ep=$enlistmentPath" "/ci=$clientId" "/cs=$clientSecret"
+& .\RIT.exe "/in=$componentName" "/vsbn=$visualStudioBranchName" /createdummypr "/u=vslsnap@microsoft.com" "/ep=$enlistmentPath" "/ci=$clientId" "/cs=$clientSecret" "/wpr=$writePullRequest"
