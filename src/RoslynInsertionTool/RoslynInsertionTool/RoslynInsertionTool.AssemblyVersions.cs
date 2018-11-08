e824b8d03b334e8a8f1ef89691b40cedeecafc53
Brett V. Forsgren <brettfo@microsoft.com>
2018-11-07 18:54:18 -0800

    version numbers don't always have the 'Version=' prefix (#416)

--- src/RoslynInsertionTool/RoslynInsertionTool/RoslynInsertionTool.AssemblyVersions.cs
+++ src/RoslynInsertionTool/RoslynInsertionTool/RoslynInsertionTool.AssemblyVersions.cs
@@ -9,6 +9,8 @@ namespace Roslyn.Insertion
 {
     static partial class RoslynInsertionTool
     {
+        private const string VersionEqualsPrefix = "Version=";
+
         private static void UpdateAssemblyVersions(string artifactsFolder)
         {
             var versionsUpdater = new VersionsUpdater(GetAbsolutePathForEnlistment(), WarningMessages);
@@ -25,7 +27,9 @@ namespace Roslyn.Insertion
         {
             return from line in File.ReadAllLines(path)
                    let columns = line.Split(',')
-                   let version = Version.Parse(columns[1].Trim().Substring("Version=".Length)) // trim Version= from the start
+                   let versionFull = columns[1].Trim()
+                   let versionProper = versionFull.StartsWith(VersionEqualsPrefix) ? versionFull.Substring(VersionEqualsPrefix.Length) : versionFull
+                   let version = Version.Parse(versionProper)
                    let fullVersion = new Version(version.Major, Math.Max(version.Minor, 0), Math.Max(version.Build, 0), Math.Max(version.Revision, 0))
                    select new KeyValuePair<string, Version>(columns[0].Trim(), fullVersion);
         }
