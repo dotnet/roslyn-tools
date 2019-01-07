3734a8f1f37bd509453265763d60531dc46e790e
Tomáš Matoušek <tmat@users.noreply.github.com>
2019-01-07 22:54:06 +0100

    Simplify implementation of roslyn.optprof.runsettings.generator. (#444)
    Remove implicit dependency on various environment variables.
    Remove unused code.

--- src/OptProf/roslyn.optprof.runsettings.generator/Program.cs
+++ src/OptProf/roslyn.optprof.runsettings.generator/Program.cs
@@ -4,12 +4,12 @@ using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using roslyn.optprof.json;
 using roslyn.optprof.lib;
-using YamlDotNet.RepresentationModel;
 
 namespace roslyn.optprof.runsettings.generator
 {
@@ -24,300 +24,133 @@ namespace roslyn.optprof.runsettings.generator
                 .UseParseErrorReporting()
                 .UseExceptionHandler()
                 .AddOption(
-                    new[] { "-c", "--configFile" },
+                    new[] { "-c", "--config" },
                     "REQUIRED: The absolute path to the OptProf.json config file.",
                     c => c.WithDefaultValue(() => null).LegalFilePathsOnly().ExistingFilesOnly().ParseArgumentsAs<string>())
                 .AddOption(
-                    new[] { "-o", "--outputFolder" },
-                    "REQUIRED: The folder to write the run settings file to.",
+                    new[] { "-o", "--out" },
+                    "REQUIRED: The file path to write the run settings to (e.g. 'RoslynOptProf.runsettings').",
                     o => o.WithDefaultValue(() => null).LegalFilePathsOnly().ParseArgumentsAs<string>())
                 .AddOption(
-                    new[] { "-tp", "--teamProject" },
-                    "optinal override, otherwise picked up from environment variables.",
+                    new[] { "-d", "--vsDropName" },
+                    "REQUIRED: Product drop name, e.g. 'Products/$(System.TeamProject)/$(Build.Repository.Name)/$(Build.SourceBranchName)/$(Build.BuildNumber)'",
                     p => p.WithDefaultValue(() => null).ParseArgumentsAs<string>())
                 .AddOption(
-                    new[] { "-rn", "--repoName" },
-                    "optinal override, otherwise picked up from environment variables.",
-                    r => r.WithDefaultValue(() => null).ParseArgumentsAs<string>())
-                .AddOption(
-                    new[] { "-sbn", "--sourceBranchName" },
-                    "optinal override, otherwise picked up from environment variables.",
-                    s => s.WithDefaultValue(() => null).ParseArgumentsAs<string>())
-                .AddOption(
-                    new[] { "-bi", "--buildId" },
-                    "optinal override, otherwise picked up from environment variables.",
-                    i => i.WithDefaultValue(() => null).ParseArgumentsAs<string>())
-                .AddOption(
-                    new[] { "-t", "--testsUrl" },
-                    "optinal override, otherwise picked up from environment variables.",
+                    new[] { "-b", "--bootstrapperInfo" },
+                    "REQUIRED: Path to the BootstrapperInfo.json",
                     b => b.WithDefaultValue(() => null).ParseArgumentsAs<string>())
-                .AddVersionOption()
                 .OnExecute(typeof(Program).GetMethod(nameof(ExecuteAsync)))
                 .Build();
 
             return await parser.InvokeAsync(args);
         }
 
-        public static async Task<int> ExecuteAsync(string configFile,
-                                              string outputFolder,
-                                              string teamProject,
-                                              string repoName,
-                                              string sourceBranchName,
-                                              string buildId,
-                                              string testsUrl,
-                                              IConsole console = null)
-        {
-            await ValidateAsync(configFile, nameof(configFile), console);
-            await ValidateAsync(outputFolder, nameof(outputFolder), console);
-            if (configFile == null || outputFolder == null)
-            {
-                return 1;
-            }
-            var fileWriter = new FileWriter();
-
-            using (var config = File.OpenRead(configFile))
-            {
-                return Execute(config, configFile, outputFolder, teamProject, repoName, sourceBranchName, buildId, testsUrl, fileWriter, console);
-            }
-        }
-
-        public static int Execute(Stream config,
-                                  string configPath,
-                                  string outputFolder,
-                                  string teamProject,
-                                  string repoName,
-                                  string sourceBranchName,
-                                  string buildId,
-                                  string testsUrl,
-                                  IFileWriter fileWriter,
-                                  IConsole console = null)
-        {
-            var dropUriString = GetDropUriString(teamProject, repoName, sourceBranchName, buildId);
-
-            var buildUriString = GetBuildUriString(testsUrl);
-            if (buildUriString == null)
-            {
-                var stagingDirectory = Environment.GetEnvironmentVariable("BUILD_STAGINGDIRECTORY");
-                var bootstrapperInfoPath = Path.Combine(stagingDirectory, @"MicroBuild\Output\BootstrapperInfo.json");
-
-                console?.Error.WriteLine($"unable to get the build URL");
-                console?.Error.WriteLine($"BUILD_STAGINGDIRECTORY: '{stagingDirectory}'");
-                var fileExists = File.Exists(bootstrapperInfoPath);
-                console?.Error.WriteLine($"BootstrapperInfo Path: '{bootstrapperInfoPath}' Exists: '{fileExists}'");
-                if (fileExists)
-                {
-                    console?.Error.WriteLine($"BootstrapperInfo Contents: {Environment.NewLine}{File.ReadAllText(bootstrapperInfoPath)}");
-                }
-                return 1;
-            }
-
-            var (success, testContainerString, testCaseFilterString) = GetContainerString(config);
-            if (!success)
-            {
-                console?.Error.WriteLine($"unable to read config file '{configPath}'");
-                return 1;
-            }
-
-            var runSettings = string.Format(Constants.RunSettingsTemplate, dropUriString, buildUriString, testContainerString, testCaseFilterString);
-
-            return fileWriter.WriteOutFile(outputFolder, runSettings);
-        }
-
-
-
-        private static string GetBuildUriString(string testsUrl)
-        {
-            bool success;
-            if (testsUrl != null)
-            {
-                return $"vstsdrop:{testsUrl}";
-            }
-            else
-            {
-                (success, testsUrl) = GetTestsUrl();
-            }
-
-            return success ? testsUrl : null;
-        }
-
-        public static (bool, string) GetTestsUrl(string bootstrapperInfoPath = null)
-        {
-            if (bootstrapperInfoPath == null)
-            {
-                var stagingDirectory = Environment.GetEnvironmentVariable("BUILD_STAGINGDIRECTORY");
-                if (string.IsNullOrEmpty(stagingDirectory))
-                {
-                    return (false, null);
-                }
-
-                bootstrapperInfoPath = Path.Combine(stagingDirectory, @"MicroBuild\Output\BootstrapperInfo.json");
-            }
-
-            using (var file = File.OpenText(bootstrapperInfoPath))
-                return GetTestsUrl(file);
-        }
-
-        public static (bool, string) GetTestsUrl(StreamReader file)
+        public static Task<int> ExecuteAsync(
+            string config,
+            string @out,
+            string vsDropName,
+            string bootstrapperInfo,
+            IConsole console = null)
         {
             try
             {
-                using (var reader = new JsonTextReader(file))
+                RequireValue(config, nameof(config));
+                RequireValue(@out, nameof(@out));
+                RequireValue(vsDropName, nameof(vsDropName));
+                RequireValue(bootstrapperInfo, nameof(bootstrapperInfo));
+
+                WriteOutFile(@out, GenerateRunSettings(config, vsDropName, bootstrapperInfo));
+                return Task.FromResult(0);
+            }
+            catch (Exception e)
+            {
+                console?.Error.WriteLine(e.Message);
+                return Task.FromResult(1);
+            }
+        }
+
+        private static void RequireValue(string value, string argumentName)
+        {
+            if (value == null)
+            {
+                throw new ApplicationException($"Argument required: --{argumentName}");
+            }
+        }
+
+        public static void WriteOutFile(string filePath, string content)
+        {
+            var dir = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            File.WriteAllText(filePath, content);
+        }
+
+        public static string GenerateRunSettings(
+            string configPath,
+            string vsDropName, 
+            string bootstrapperInfoPath)
+        {
+            var profilingInputsDropName = GetProfilingInputsDropName(vsDropName);
+            var buildDropName = GetTestsDropName(File.ReadAllText(bootstrapperInfoPath, Encoding.UTF8));
+            var (testContainerString, testCaseFilterString) = GetContainerString(File.ReadAllText(configPath), configPath);
+
+            return string.Format(
+                Constants.RunSettingsTemplate,
+                "vstsdrop:" + profilingInputsDropName,
+                "vstsdrop:" + buildDropName,
+                testContainerString,
+                testCaseFilterString);
+        }
+
+        public static string GetTestsDropName(string bootstrapperInfoJson)
+        {
+            try
+            {
+                var jsonContent = JToken.Parse(bootstrapperInfoJson);
+                var dropUrl = (string)((JArray)jsonContent).First["BuildDrop"];
+
+                const string prefix = "https://vsdrop.corp.microsoft.com/file/v1/Products/";
+                if (!dropUrl.StartsWith(prefix, StringComparison.Ordinal))
                 {
-                    var jsonContent = JToken.ReadFrom(reader);
-                    var buildDropPath = (string)((JArray)jsonContent).First["BuildDrop"];
-                    if (buildDropPath.Contains("Products") && buildDropPath.Contains("https://vsdrop.corp.microsoft.com/file/v1/"))
-                    {
-                        var testsUri = $"vstsdrop:{buildDropPath.Replace("Products", "Tests").Substring("https://vsdrop.corp.microsoft.com/file/v1/".Length)}";
-                        return (true, testsUri);
-                    }
-                    else
-                    {
-                        return (false, null);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                return (false, null);
-            }
-        }
-
-        private static string GetDropUriString(string teamProject, string repoName, string sourceBranchName, string buildId)
-        {
-            if (teamProject == null)
-            {
-                teamProject = Environment.GetEnvironmentVariable("SYSTEM_TEAMPROJECT");
-            }
-
-            if (repoName == null)
-            {
-                repoName = Environment.GetEnvironmentVariable("BUILD_REPOSITORY_NAME");
-            }
-
-            if (sourceBranchName == null)
-            {
-                sourceBranchName = Environment.GetEnvironmentVariable("BUILD_SOURCEBRANCHNAME");
-            }
-
-            if (buildId == null)
-            {
-                buildId = Environment.GetEnvironmentVariable("BUILD_BUILDID");
-            }
-
-            var dropUriString = $"vstsdrop:ProfilingInputs/{teamProject}/{repoName}/{sourceBranchName}/{buildId}";
-            return dropUriString;
-        }
-
-        private static async Task ValidateAsync(string option, string optionName, IConsole console)
-        {
-            if (option == null && console != null)
-            {
-                await console.Error.WriteLineAsync($"You must specify '--{optionName}'");
-            }
-        }
-
-        private static string GetTargetBranch(string yamlFileName)
-        {
-            var sourcesRoot = Environment.GetEnvironmentVariable("BUILD_SOURCESDIRECTORY");
-            var yamlFile = Path.Combine(sourcesRoot, yamlFileName);
-            using (var stream = File.OpenText(yamlFile))
-            {
-                var yaml = new YamlStream();
-                yaml.Load(stream);
-                return GetTargetBranch(yaml);
-            }
-        }
-
-        public static string GetTargetBranch(YamlStream yaml)
-        {
-            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            // The "variables" node can be stored in two places: root of YAML and under the
-            // jobs+jobs path. Need to look in both places here.
-            var branchNode = tryGetBranch(mapping);
-            if (branchNode != null)
-            {
-                return (string)branchNode;
-            }
-
-            if (mapping.Children.TryGetValue("jobs", out var node) && node is YamlSequenceNode jobs)
-            {
-                foreach (var job in jobs.OfType<YamlMappingNode>())
-                {
-                    branchNode = tryGetBranch(job);
-                    if (branchNode != null)
-                    {
-                        return (string)branchNode;
-                    }
-                }
-            }
-
-            throw new Exception("Unable to calculate the target branch");
-
-            YamlNode tryGetBranch(YamlMappingNode yamlNode)
-            {
-                return tryGetPath(yamlNode, "variables", "InsertTargetBranchFullName");
-            }
-
-            YamlNode tryGetPath(YamlMappingNode yamlNode, params string[] path)
-            {
-                YamlNode current = yamlNode;
-                for (var i = 0; i < path.Length; i++)
-                {
-                    if (!(current is YamlMappingNode mappingNode))
-                    {
-                        return null;
-                    }
-
-                    if (!mappingNode.Children.TryGetValue(path[i], out current))
-                    {
-                        return null;
-                    }
+                    throw new ApplicationException($"Invalid drop URL: '{dropUrl}'");
                 }
 
-                return current;
+                return $"Tests/{dropUrl.Substring(prefix.Length)}";
+            }
+            catch (Exception e)
+            {                
+                throw new InvalidDataException(
+                    $"Unable to read boostrapper info: {e.Message}{Environment.NewLine}" +
+                    $"Content of BootstrapperInfo.json:{Environment.NewLine}" +
+                    $"{bootstrapperInfoJson}");
             }
         }
 
-        private static (bool, string) GetBuildNumber()
+        private static string GetProfilingInputsDropName(string vsDropName)
         {
-            var stagingDirectory = Environment.GetEnvironmentVariable("BUILD_STAGINGDIRECTORY");
-            if (string.IsNullOrEmpty(stagingDirectory))
+            const string prefix = "Products/";
+            if (!vsDropName.StartsWith(prefix, StringComparison.Ordinal))
             {
-                return (false, null);
+                throw new ApplicationException("Invalid value of vsDropName argument: must start with 'Products/'.");
             }
 
-            var bootstrapperInfoPath = Path.Combine(stagingDirectory, @"MicroBuild\Output\BootstrapperInfo.json");
-
-            using (var file = File.OpenText(bootstrapperInfoPath))
-            using (var reader = new JsonTextReader(file))
-            {
-                var jsonContent = JToken.ReadFrom(reader);
-                var parts = ((string)((JArray)jsonContent).First["VSBuildVersion"]).Split('.');
-                return (true, parts[2] + "." + parts[3]);
-            }
+            return "ProfilingInputs/" + vsDropName.Substring(prefix.Length);
         }
 
-        private static (bool, string, string) GetContainerString(Stream config)
+        public static (string containers, string filters) GetContainerString(string configJson, string configPath)
         {
-            using (var reader = new StreamReader(config))
+            try
             {
-                return GetContainerString(reader);
+                var config = Config.ReadConfigFile(configJson);
+                return (GetTestContainers(config), GetTestFilters(config));
             }
-        }
-
-        public static (bool, string, string) GetContainerString(StreamReader file)
-        {
-            var (success, config) = Config.TryReadConfigFile(file);
-            if (!success)
+            catch (Exception e)
             {
-                return (false, null, null);
+                throw new ApplicationException($"Unable to read config file '{configPath}': {e.Message}");
             }
-
-            var containers = GetTestContainers(config);
-            var filters = GetTestFilters(config);
-
-            return (true, containers, filters);
         }
 
         private static string GetTestContainers(OptProfTrainingConfiguration config)
