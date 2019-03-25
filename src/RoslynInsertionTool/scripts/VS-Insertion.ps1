800692bd45b8388d13024f6b686099c6332a0215
Joey Robichaud <jorobich@microsoft.com>
2019-03-25 15:03:42 -0700

    Update merges and insertions for 16.1 preview 1

--- src/RoslynInsertionTool/scripts/VS-Insertion.ps1
+++ src/RoslynInsertionTool/scripts/VS-Insertion.ps1
@@ -47,18 +47,21 @@ function Do-Insertion(
 
 # Dev15.8 - Servicing only
 #Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "dev15.8.x-vs-deps" -toBranch "rel/d15.8" -insertToolset "false" -insertDevdiv "false" -queueValidation "true" -updatecorextl#ibraries "true"
-#Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev15.8.x"         -toBranch "rel/d15.8" -insertCore "false" -insertDevdiv "false" -queueValidation "true"
-#Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev15.8.x"         -toBranch "rel/d15.8" -insertCore "true"                       -queueValidation "true" -dropPath "server"
+#Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev15.8.x"         -toBranch "rel/d15.8" -insertCore "false"    -insertDevdiv "false" -queueValidation "true"
+#Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev15.8.x"         -toBranch "rel/d15.8" -insertCore "true"                           -queueValidation "true" -dropPath "server"
 
 # Dev15.9
 Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "dev15.9.x-vs-deps"        -toBranch "rel/d15.9"    -insertToolset "false" -insertDevdiv "false"   -updatecorextlibraries "true" -queueValidation "true"
 
-# Dev16 RC
+# Dev16.0 RC
 #Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "dev16.0-vs-deps"          -toBranch "rel/d16.0"    -insertToolset "false" -insertDevdiv "false"   -updatecorextlibraries "true" -queueValidation "true"
-#Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev16.0.x"                -toBranch "rel/d16.0" -insertCore "false"    -insertDevdiv "false"   -queueValidation "true"
-Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev16.0"                   -toBranch "rel/d16.0" -insertCore "true"     -queueValidation "true" -dropPath "server"
+#Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "dev16.0.x"                -toBranch "rel/d16.0"    -insertCore "false"    -insertDevdiv "false"   -queueValidation "true"
+Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev16.0"                  -toBranch "rel/d16.0"    -insertCore "true"     -queueValidation "true" -dropPath "server"
 
 # Dev16.1 Preview 1
+Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "dev16.1-preview1-vs-deps" -toBranch "rel/d16.1"    -insertToolset "true"  -insertDevdiv "false"   -updatecorextlibraries "true" -queueValidation "true"
+
+# Dev16.1 Preview 2
 Do-Insertion -component "Roslyn"            -queueName "Roslyn-Signed"         -fromBranch "master-vs-deps"           -toBranch "lab/d16.1stg" -insertToolset "true"  -insertDevdiv "false"   -updatecorextlibraries "true" -queueValidation "true"
 #Do-Insertion -component "Live Unit Testing" -queueName "TestImpact-Signed"     -fromBranch "master"                   -toBranch "lab/d16.1stg" -insertCore "false"    -insertDevdiv "false"   -queueValidation "true"
-#Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev16.0.x"                -toBranch "lab/d16.1stg" -insertCore "true"     -queueValidation "true" -dropPath "server"
+#Do-Insertion -component "VS Unit Testing"   -queueName "VSUnitTesting-Signed"  -fromBranch "dev16.1.x"                -toBranch "lab/d16.1stg" -insertCore "true"     -queueValidation "true" -dropPath "server"
