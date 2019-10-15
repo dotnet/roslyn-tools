ab9e49f1f418dc49bb4a39c7eea4f0ddc0e6d148
David Barbet <dibarbet@gmail.com>
2019-10-15 13:33:40 -0700

    Create new project for creating github merge PRs

---
+++ src/GitHubCreateMergePRs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubCreateMergePRs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
