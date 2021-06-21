9841051fa7f55b63301a4acdb23d5380a1694385
Daniel Monroy <dmon@microsoft.com>
2021-06-21 12:38:00 -0700

    Use the reviewerGUID as the user ID instead of MLInfraSwatUserId

--- src/RoslynInsertionTool/RoslynInsertionTool/RoslynInsertionTool.cs
+++ src/RoslynInsertionTool/RoslynInsertionTool/RoslynInsertionTool.cs
@@ -24,7 +24,7 @@ namespace Roslyn.Insertion
         public static readonly Guid VSRepoId = new Guid("a290117c-5a8a-40f7-bc2c-f14dbe3acf6d");
         //Easiest way to get these GUIDs is to create a PR search in AzDo
         //You'll get something like https://dev.azure.com/devdiv/DevDiv/_git/VS/pullrequests?_a=active&createdBy=GUID-here
-        public static readonly Guid MLInfraSwatUserId = new Guid("6c25b447-1d90-4840-8fde-d8b22cb8733e");
+        public static readonly Guid ReviewerUserId = new Guid($"{Options.ReviewerGUID}");
         public static readonly Guid VSLSnapUserId = new Guid("9f64bc2f-479b-429f-a665-fec80e130b1f");
 
         private static List<string> WarningMessages { get; } = new List<string>();
@@ -441,7 +441,7 @@ namespace Roslyn.Insertion
 
                         var reviewerId = isPrValidation || isDevOrFeatureBranch
                             ? buildToInsert.RequestedBy.Id
-                            : MLInfraSwatUserId.ToString();
+                            : ReviewerUserId.ToString();
 
                         pullRequest = await CreateVSPullRequestAsync(insertionBranchName, prDescriptionMarkdown, buildVersion.ToString(), options.TitlePrefix, reviewerId, cancellationToken);
                         if (pullRequest == null)
