d1c5de50e780d0cbe55c823de6f020528b2387a5
Rikki Gibson <rigibson@microsoft.com>
2021-10-19 13:31:15 -0700

    Remove unused keyvault-related projects

--- src/AzureFunctionPackage/CreateZipPackage.ps1
+++
@@ -1,7 +0,0 @@
-param (
-    [string]$sourceDirectoryName,
-    [string]$destinationArchiveFileName
-)
-
-Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
-[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName)
